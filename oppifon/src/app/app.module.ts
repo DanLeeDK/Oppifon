2286c0765187dae2a3ac64e0dce4e547bafa800a
Dan Lee <danjhinlee@hotmail.com>
2018-09-12 10:38:38 +0200

    Components login and home

--- oppifon/src/app/app.module.ts
+++ oppifon/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule
