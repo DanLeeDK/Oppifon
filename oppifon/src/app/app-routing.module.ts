cbf93a2a02773a0ab656707c862cfe6d6a6d0e01
Dan Lee <danjhinlee@hotmail.com>
2018-09-12 11:35:19 +0200

    Routing setup

---
+++ oppifon/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { UserHomeComponent } from './user-home/user-home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'userhome', component: UserHomeComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- oppifon/src/app/app.module.ts
+++ oppifon/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserHomeComponent } from './user-home/user-home.component';
+
+import { AppRoutingModule } from './/app-routing.module';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { UserHomeComponent } from './user-home/user-home.component';
     UserHomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
