d6df46c7d610dfac37cd20e8e97aa20771db9d7c
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2018-09-14 10:13:43 +0200

    more cards

--- oppifon/src/app/sign-up/sign-up.component.ts
+++ oppifon/src/app/sign-up/sign-up.component.ts
@@ -5,11 +5,51 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
+
 export class SignUpComponent implements OnInit {
+  public showWhoAreYou;
+  public showWhatAreYou;
+  public showAreYouAnExpert;
+  public showExpertCategories;
+  public test;
 
   constructor() { }
 
   ngOnInit() {
+    this.showWhoAreYou = true;
+    this.showWhatAreYou = false;
+    this.showAreYouAnExpert = false;
+    this.showExpertCategories = false;
+    this.test = "sdfasdf"
   }
 
+  public showWhoAreYouClick(){
+    this.showWhoAreYou = true;
+    this.showWhatAreYou = false;
+    this.showAreYouAnExpert = false;
+    this.showExpertCategories = false;
+  }
+
+  public showWhatAreYouClick(){
+    this.showWhoAreYou = false;
+    this.showWhatAreYou = true;
+    this.showAreYouAnExpert = false;
+    this.showExpertCategories = false;
+    this.test = "CLICK";
+    console.log("click");
+  }
+
+  public showAreYouAnExpertClick(){
+    this.showWhoAreYou = false;
+    this.showWhatAreYou = false;
+    this.showAreYouAnExpert = true;
+    this.showExpertCategories = false;
+  }
+
+  public showExpertCategorysClick(){
+    this.showWhoAreYou = false;
+    this.showWhatAreYou = false;
+    this.showAreYouAnExpert = false;
+    this.showExpertCategories = true;
+  }
 }
