b9bb9a60e61fc8211c8adb21b76154324ecad26f
Dan Lee <danjhinlee@hotmail.com>
2018-09-27 09:34:21 +0200

    Expert page done

--- oppifon/src/app/expert-profile/expert-profile.component.ts
+++ oppifon/src/app/expert-profile/expert-profile.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expert-profile.component.scss']
 })
 export class ExpertProfileComponent implements OnInit {
-
+public reviews = ['Super professional help', 'Really helped me a lot', 'Best talk ever!'];
   constructor() { }
 
   ngOnInit() {
