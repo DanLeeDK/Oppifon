8c61b9fc00adb63d008586111b1030aa11e32b36
Dan Lee <danjhinlee@hotmail.com>
2018-09-27 13:42:53 +0200

    Search init

--- oppifon/src/app/search/search.component.ts
+++ oppifon/src/app/search/search.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  public mainOptions = ['Technologi', 'Health', 'Education', 'Sport', 'Psychology'];
+  public mainCatagory = '';
+  public subOptions = [];
+  public subCatagory = '';
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  public getSubCatagories(mainCatagory: string) {
+    // TODO service call
+    this.subOptions = ['Love', 'Bullying', 'Stress', 'Anxiety', 'Food'];
+    console.log('Hello');
   }
 
 }
